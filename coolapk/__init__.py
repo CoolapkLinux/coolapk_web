c62c8d0aed9f8dbe792e5dc1df1170d092dcd163
purofle <3272912942@qq.com>
2021-01-31 19:32:20 +0800

    好耶是话题

--- coolapk/__init__.py
+++ coolapk/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from coolapk.coolapk import bp
+from coolapk.topic import bp as bp_topic
 
 
 def create_app() -> Flask:
@@ -11,7 +12,10 @@ def create_app() -> Flask:
         SECRET_KEY='dev'
     )
 
+    # 主页
     app.register_blueprint(bp)
     app.add_url_rule("/", endpoint="index")
+    # 话题
+    app.register_blueprint(bp_topic)
 
     return app
--- coolapk/topic.py
+++ coolapk/topic.py
@@ -2,4 +2,8 @@ from flask import (
         Blueprint
         )
 
-bp = Blueprint("topic", __name__, url_prefix="/t/<string:topic_name>")
+bp = Blueprint("topic_page", __name__)
+
+@bp.route("/t/<string:topic_n>")
+def topic_page(topic_n):
+    return topic_n
