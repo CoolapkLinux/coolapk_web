c3d73ff29d83d9978415cabea5557dfc75ccde5d
purofle <purofle@qq.com>
2021-10-04 18:54:07 +0800

    init

--- coolapk/topic.py
+++
@@ -1,9 +0,0 @@
-from flask import (
-        Blueprint
-        )
-
-bp = Blueprint("topic_page", __name__)
-
-@bp.route("/t/<string:topic_n>")
-def topic_page(topic_n):
-    return topic_n
--- main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from coolapk import create_app
-
-app = create_app()
-
-app.debug = True
-app.run("0.0.0.0")
