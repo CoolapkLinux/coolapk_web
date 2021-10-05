b6e8b5d8f04b7440200abe6a5c0c410e46f27992
purofle <purofle@qq.com>
2021-10-05 20:42:02 +0800

    init

---
+++ api/token.ts
@@ -0,0 +1,17 @@
+import * as Base64 from 'base-64'
+import md5 from 'md5'
+
+function get_token() {
+	var DEVICE_ID = "8513efac-09ea-3709-b214-95b366f1a185"
+
+	var t = Date.now()
+	// 将时间戳截到10位再转16进制
+	var hex_t = "0x"+Number(t.toFixed().substring(0, 10)).toString(16)
+	var md5_t = md5(t.toFixed())
+	var a = `token://com.coolapk.market/c67ef5943784d09750dcfbb31020f0ab?${md5_t}${DEVICE_ID}&com.coolapk.market`
+
+	var md5_a = md5(Base64.encode(a))
+	return `${md5_a}${DEVICE_ID}${hex_t}`
+}
+
+console.log(get_token())
--- coolapk/src/App.vue
+++ coolapk/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Coolapk />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Coolapk from './components/Coolapk.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Coolapk
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
