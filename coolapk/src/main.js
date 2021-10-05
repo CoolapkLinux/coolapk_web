6b31852b13aa9781896e36dd185ad8559f213d98
purofle <purofle@qq.com>
2021-10-05 21:29:55 +0800

    引入mdui

--- coolapk/src/main.js
+++ coolapk/src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import 'mdui/dist/css/mdui.min.css'
+import 'mdui/dist/js/mdui.min.js'
 
 const app = createApp(App)
-app.use(ElementPlus)
 app.mount('#app')
